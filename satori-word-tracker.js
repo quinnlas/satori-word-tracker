7bf00855b2dfa5defa3d219069fc03c7445db857
Quinn Las <quinn.s.las@gmail.com>
2024-09-22 13:21:56 -0500

    add word highlight and percent calc

--- satori-word-tracker.js
+++ satori-word-tracker.js
@@ -1,5 +1,6 @@
 console.log("LOADED content script for Satori Word Tracker")
 
+// DATA ------------
 let knownWords
 
 async function loadStorage() {
@@ -7,13 +8,14 @@ async function loadStorage() {
   knownWords = storedKnownWords.knownWords
 }
 
+// DISPLAY ------------
 function getWordText(word) {
   return Array.from(word.getElementsByClassName("wpr"))
     .map((wpr) => wpr.textContent)
     .join("")
 }
 
-function setKanjiClasses() {
+function setWordClasses() {
   for (let word of document.getElementsByClassName("word")) {
     // clear existing kanji knowledge classes from the word element
     word.classList.remove(
@@ -34,6 +36,21 @@ function setKanjiClasses() {
   }
 }
 
+function calcPercent() {
+  let known = 0
+  let total = 0
+  for (let word of document.getElementsByClassName("word")) {
+    const wordText = getWordText(word)
+
+    if (knownWords[wordText]) known++
+    total++
+  }
+
+  const displayPercent = Math.round(1000 * known / total) / 10
+  const displayString = `${displayPercent}%`
+  document.getElementsByClassName("word-tracker-percent")[0].textContent = displayString
+}
+
 function getSelectedWordText() {
   const selectedWord = document.getElementsByClassName("word-selected")[0]
   return getWordText(selectedWord)
@@ -48,7 +65,7 @@ function setupWordManagementButtons() {
     knownWords[wordText] = true
     await chrome.storage.sync.set({ knownWords })
 
-    setKanjiClasses()
+    refreshDisplay()
   }
 
   async function markWordAsUnknown() {
@@ -57,7 +74,7 @@ function setupWordManagementButtons() {
     delete knownWords[wordText]
     await chrome.storage.sync.set({ knownWords })
 
-    setKanjiClasses()
+    refreshDisplay()
   }
 
   const addButton = document.createElement("button")
@@ -74,7 +91,25 @@ function setupWordManagementButtons() {
   tooltipElem.prepend(addButton)
 }
 
+function setupPercentDisplay() {
+  const element = document.createElement("span")
+  element.classList.add("word-tracker-percent")
+  element.textContent = "...%"
+  document.getElementsByClassName("article-title")[0].prepend(element)
+}
+
+function refreshDisplay() {
+  setWordClasses()
+  calcPercent()
+}
+
+function startDisplay() {
+  setupWordManagementButtons()
+  setupPercentDisplay()
+  refreshDisplay()
+}
+
+// MAIN ------------
 loadStorage()
-  .then(() => setKanjiClasses())
+  .then(() => startDisplay())
   .catch(console.error)
-setupWordManagementButtons()
